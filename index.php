0df2e96e33270935420e54066fafeb3ce08860d3
nkhash <103872134+nkhash@users.noreply.github.com>
2022-12-28 13:32:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ $url =$_GET['c'];
 if($url !=""){
 $id = end(explode('/', $url));
 $tlink ="https://apiv2.sonyliv.com/AGL/3.0/A/ENG/MWEB/IN/JH/CONTENT/VIDEOURL/VOD/$url/freepreview";
-$tokenurl =file_get_contents("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app");
-$tok =json_decode($tokenurl, true);
-$token =$tok['token'];
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -16,7 +13,7 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
-    "Security_Token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjgzMDc2IiwiZW1haWwiOiJzcG5zcjRtZUBnbWFpbC5jb20iLCJ0aW1lc3RhbXAiOjE2NzIyMDQyMDd9.C1fEkf-J5SVkojBPGTLqgUSHKjpF30zlGpYL3VLhtNI",
+    "Security_Token: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzIyMTE5MTQsImV4cCI6MTY3MzUwNzkxNCwiYXVkIjoiKi5zb255bGl2LmNvbSIsImlzcyI6IlNvbnlMSVYiLCJzdWIiOiJzb21lQHNldGluZGlhLmNvbSJ9.PUgmKteYphsKnk7naE98dh9hnhNrRNlzHoZwmmnkK6jA8up7pxe62LXWobu2U5Q_jCXFz4RR642aeGXgTS9xPmqgyxwqv3E-EVjqvVvtESLTp9VAwjf57YKXHhs9Z4aLpCDSyDltfCtSOQQh8qLQMd177MsbraJ2nco_M5p8YMkDrXKj79irMSg1oZR0UyFlIjsjht0HBvCAE0or1U3fDyjKy4qzr2L3HJDi9A0Bj82rfQenBLtix7bDfeyNJEvVp0r86KP_sAoiUoc0pfnbU4Xti-MhX0AGK0JBAXJhVFV8udgeFsxorAdeXz5PpCGrbjQ_3NJIylZ78ATlCdzjG7SNBH4N2xUUaIciwKZK2mTUeHtSoW6w9oJkWUxuYzAoGhngO8rxNNqGyJ8aNqjNQyWe1dlEq28JrQVY08KhB5QJ7LLzh_UeaGoF4gSshyq05b1KPe8hXumFiaTaD-bHtdiucs2kDTCoqyO2dJy3ei4YSCfoxOnvb0uLBoZxXSIVSMiKVNmnMyClL9Zd1OXymwyqG9WFDYe3S51vKQUK1Lono3vBr7ezq5JMM8jVRzrBurUtqpY8yVWZQW5PoWYaH828nyCzJ6NmOZsNa_jPxVNydLwK8Rd9CQWbtuyM-CGSNj2wiPNK6nEh-HipyVDjcgySoyl77-YOoHgiyjdK4_c",
     "Content-Type: application/json"
   ),
 ));
